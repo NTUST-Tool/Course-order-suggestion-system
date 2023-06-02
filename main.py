56e7c1d8c2e2f216c0ce5835146f6e0557c10532
CCcat <linnemo920317@gmail.com>
2023-06-02 20:57:33 +0800

    fix bug

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def main(course_list: list[str]):
         restrict = int(x['Restrict2'])
         if all > restrict:
             x['choise_rate'] = round(
-                comb(all-1, restrict-1)/comb(all, restrict), 2)
+                comb(all-1, restrict-1)/comb(all, restrict), 2)*100
         else:
             x['choise_rate'] = 100
     courses.sort(key=lambda x: x['choise_rate'])
